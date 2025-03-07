8ab1206b67ffef2c5d59414048cdc97ef29bd1bd
Dannika <71364766+dannikate@users.noreply.github.com>
2025-03-07 11:02:41 +1000

    update backend url, scripts

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,13 @@
 import './style.css';
 
+const API_BASE_URL = 'https://my-fullstack-app-hazel.vercel.app/';
+
 const app = document.getElementById('app');
 
 // fetch data from the backend 
 async function fetchData() {
   try {
-    const response = await fetch('/api');
+    const response = await fetch(`${API_BASE_URL}/api`);
     const data = await response.json();
     app.innerHTML =`<h1>${data.message}</h1><hr><p>(printing the message from the frontend)</p>`;
   } catch (error) {
